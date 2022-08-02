6c3455923965d18ffda623723a370314b355f210
Tomasz <tomasz.majewski1994@gmail.com>
2022-08-02 19:05:20 +0200

    fix fonts and add eventhandler

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import UsersList from 'components/organisms/UsersList/UsersList';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'assets/icons/styles/GlobalStyle';
-import { theme } from 'assets/icons/styles/theme';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
 import { Wrapper } from './Root.styles';
 
 const Root = () => (
